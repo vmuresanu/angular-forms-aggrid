862c6f76ba5ec750f3d68ffc4241dcc9759988be
Victor Muresanu <Victor.Muresanu@endava.com>
2020-06-06 17:13:09 +0300

    Add aggrid to the project

---
+++ aggrid-angular/src/app/grid/grid.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { AgGridAngular } from 'ag-grid-angular';
+import { GridOptions } from 'ag-grid-community';
+
+@Component({
+  selector: 'app-grid',
+  templateUrl: './grid.component.html',
+  styleUrls: ['./grid.component.scss']
+})
+export class GridComponent implements OnInit {
+  @ViewChild('agGrid') agGrid: AgGridAngular;
+  title = 'app';
+
+  columnDefs = [
+    {headerName: 'Make', field: 'make', sortable: true, filter: true,  checkboxSelection: true },
+    {headerName: 'Model', field: 'model', sortable: true, filter: true},
+    {headerName: 'Price', field: 'price', sortable: true, filter: true}
+  ];
+
+  rowData = [
+    { make: 'Toyota', model: 'Celica', price: 35000 },
+    { make: 'Ford', model: 'Mondeo', price: 32000 },
+    { make: 'Porsche', model: 'Boxter', price: 72000 }
+  ];
+
+  gridOptions: GridOptions = {
+    rowSelection: 'multiple'
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+  getSelectedRows() {
+    const selectedNodes = this.agGrid.api.getSelectedNodes();
+    const selectedData = selectedNodes.map( node => node.data );
+    const selectedDataStringPresentation = selectedData.map( node => node.make + ' ' + node.model).join(', ');
+    alert(`Selected nodes: ${selectedDataStringPresentation}`);
+  }
+
+}
